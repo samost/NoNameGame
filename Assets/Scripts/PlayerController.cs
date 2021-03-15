ee88662667ce868372447f6ba2f0bd26f9770ac6
samost <you@example.com>
2021-03-15 19:02:30 +0300

    test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	
+   public Joystick joystick;
+   public CharacterController controller;
+
+   private Animator _animator;
+
+   private bool _weapon;
+   
+   public float speed;
+   public float gravity;
+  
+   
+   Vector3 moveDirection;
+
+   private void Start()
+   {
+	   _animator = gameObject.GetComponent<Animator>();
+   }
+
+   void Update(){
+	  
+	   
+	   Vector2 direction = joystick.direction;
+
+	   
+	   
+	   if(controller.isGrounded)
+	   {
+            moveDirection = new Vector3(direction.x, 0, direction.y);
+			
+			Quaternion targetRotation = moveDirection != Vector3.zero ? Quaternion.LookRotation(moveDirection) : transform.rotation;
+			transform.rotation = targetRotation;
+		    moveDirection = moveDirection * speed;
+	   }
+
+	   moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+	   controller.Move(moveDirection * Time.deltaTime);
+	   
+	   if (direction != Vector2.zero)
+	   {
+	       
+		   _animator.SetInteger("State", 1);
+	   }
+	   else
+	   {
+		   _animator.SetInteger("State", 0);
+	   }
+
+	   
+	   
+	   if (Input.GetKeyUp(KeyCode.R))
+	   {
+		   _weapon = ! _weapon;
+		   _animator.SetBool("isWeapon", _weapon);
+	   }
+		
+   }
+   
+   
+   
+   
+   
+   
+}
---
+++ Assets/Scripts/ThridCameraPerson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThridCameraPerson : MonoBehaviour
+{
+
+    [SerializeField] private GameObject person;
+    private float _personPositionX;
+    private float _personPositionZ;
+    private float _personPositionY;
+
+    
+
+    void Update()
+    {
+        _personPositionX = person.transform.position.x ;
+        
+        _personPositionZ = person.transform.position.z;
+        
+        _personPositionY = person.transform.position.y;
+
+        gameObject.transform.position = new Vector3(_personPositionX + 3.312369f, _personPositionY + 4.662919f, _personPositionZ  -1.460353f);
+        
+    }
+}
